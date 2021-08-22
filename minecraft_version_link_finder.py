9efd017986e2cd1080e989015873c54e5a6ef993
MBarc <42979055+MBarc@users.noreply.github.com>
2021-08-22 12:12:52 -0400

    Create minecraft_version_link_finder.py

---
+++ minecraft_version_link_finder.py
@@ -0,0 +1,34 @@
+'''
+Purpose: When given a version number, obtain the appropriate minecraft server.jar download link
+Note: This script is designed to take inputs from a Dockerfile
+'''
+
+import sys
+import requests
+import subprocess
+
+
+# Dockerfile ENV variables go here
+userInputVersion = str(sys.argv[1])
+userInputRam = str(sys.argv[2])
+
+# This Minecraft URL gives data for each version of Minecraft
+r = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+output = r.json()
+
+url = False
+for version in output["versions"]:
+
+    if userInputVersion == version["id"]:
+
+        # This url tells us where we can download both the client and server jarfiles
+        url = version["url"]
+        r = requests.get(version["url"])
+        output = r.json()
+
+# This url is the actual server.jar download link
+url = output["downloads"]["server"]["url"]
+
+
+subprocess.run(f"wget {url} -O /server.jar", check=True)
+subprocess.run(f"java -Xms{userInputRam}G -Xmx{userInputRam}G -jar server.jar nogui", check=True)
